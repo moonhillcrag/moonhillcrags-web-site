f6fbec158115559f458e6e687fdbea6647bc4465
moonhillcrag <danielthiem21@gmail.com>
2026-03-17 18:41:15 +0100

    v1.3
    Music fix and added currently listening tab

--- moonhillcrag-edit/neocities-moonhillcrag/script.js
+++ moonhillcrag-edit/neocities-moonhillcrag/script.js
@@ -49,4 +49,37 @@ function handleSubmit(event) {
 
   const form = document.querySelector('form');
   form.addEventListener('submit', handleSubmit);
+
   
+//Last.fm Integration
+
+async function getPlaying() {
+   const url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=MoonHillCrag&api_key=b5366feafed067f237daa6fd5995542a&format=json';
+   
+   const response = await fetch(url);
+   
+   const data = await response.json();
+   
+   console.log(data);
+   
+   var current = document.getElementById("current");
+   var art = document.getElementById("albumArt");
+
+   const track = data.recenttracks.track[0];
+   const track1 = data.recenttracks.track[1];
+   const track2 = data.recenttracks.track[2];
+   const track3 = data.recenttracks.track[3];
+   const albumImage = track.image?.find(img => img.size === "extralarge")?.['#text'] || '';
+
+    current.innerHTML = `
+    <div>
+      ${albumImage ? `<img src="${albumImage}" alt="Album Art" style="width:120px;height:auto;">` : ''}
+      <p style="font-size: 25px">${track.artist['#text']} - ${track.name}</p>
+
+      <p>Recent Tracks:</p>
+      <p>${track1.artist['#text']} - ${track1.name}</p>
+      <p>${track2.artist['#text']} - ${track2.name}</p>
+      <p>${track3.artist['#text']} - ${track3.name}</p>
+      </div>
+  `;
+}
